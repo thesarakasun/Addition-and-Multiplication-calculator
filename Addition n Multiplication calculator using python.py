4cf7fb350aa9e7a98cfb28e7bef7d0f21221b374
Thesara Subasinghe <88161607+thesarakasun@users.noreply.github.com>
2022-10-15 21:00:44 +0530

    Add files via upload

---
+++ Addition n Multiplication calculator using python.py
@@ -0,0 +1,18 @@
+ans=0
+asd=1
+print("\n a)Addition \n b)Multiplication \n \n ")
+p=input("Which operation would you like to proceed(Ex: b) ?")
+q=int(input("How many operators are there? "))
+if(p=="a"):
+    for i in range(1,q+1):
+        m=int(input("Enter number " + str(i) + "?"))
+        ans=ans+m
+    print("\n Answer is : ",ans)
+elif(p=="b"):
+    for i in range(1,q+1):
+        d=int(input("Enter number " + str(i) + "?"))
+        asd=asd*d
+    print("\nAnswer is : ",asd)
+print("\n\n\nThank you for using this calculator")
+print("thesaraks.com")
+input("\n*Press ENTER to exit")
